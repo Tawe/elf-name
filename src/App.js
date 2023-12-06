2c4285491d4c08e48318a4fa609c585a072e9af4
John Munn <Munn.jon@gmail.com>
2023-12-06 14:01:53 -0400

    adding names

--- src/App.js
+++ src/App.js
@@ -4,43 +4,43 @@ import './App.css';
 const months = [
   {
     month: 'january',
-    names:['Sugar-Cookies', 'Angel-Pants', 'Sugar Plum']
+    names:['Sugar-Cookies', 'Angel-Pants', 'Sugar Plum', 'Frost-Feet']
   },
   {
     month: 'feburary',
-    names:['Jingle Bells', 'Floppy-Feet', 'Rum  -Balls']
+    names:['Jingle Bells', 'Floppy-Feet', 'Rum-Balls', 'Sugar-Loaf']
   },
   {
     month: 'march',
-    names:['Snowflake', 'Plum-Pants', 'Giggle Pants']
+    names:['Snowflake', 'Plum-Pants', 'Giggle Pants', 'Snowbells']
   },
   {
     month: 'april',
-    names:['Candy Cane', 'McJingles', 'Sleighbells']
+    names:['Candy Cane', 'McJingles', 'Sleighbells', 'Yule-Sprinkles']
   },
   {
     month: 'may',
-    names:['Peartree', 'Peppermint', 'Sparkle Pants']
+    names:['Peartree', 'Peppermint', 'Sparkle Pants', 'Jingle-Hop', 'Hollyspring']
   },
   {
     month: 'june',
-    names:['Merry Dancer', 'Toe-Bells', 'McRibbon']
+    names:['Merry Dancer', 'Toe-Bells', 'McRibbon', 'Snowybottom']
   },
   {
     month: 'july',
-    names:['Fruitcake', 'Superplum', 'Twinkle Toes']
+    names:['Fruitcake', 'Superplum', 'Twinkle Toes', 'Rosey-Cheeks']
   },
   {
     month: 'august',
-    names:['Happy Feet', 'Sugar-Socks', 'Candy Canes']
+    names:['Happy Feet', 'Sugar-Socks', 'Candy Canes', 'Jazzy-Hands']
   },
   {
     month: 'september',
-    names:['Sugar-Plums', 'Pickel-Pants', 'Sugarplum-Fairy']
+    names:['Sugar-Plums', 'Pickel-Pants', 'Sugarplum-Fairy', 'Gingersnap']
   },
   {
     month: 'october',
-    names:['Jolly-Drawers', 'Sparkley-Toes', 'Sparkle Toes']
+    names:['Jolly-Drawers', 'Sparkley-Toes', 'Sparkle Toes', 'Fluffernut']
   },
   {
     month: 'november',
@@ -54,95 +54,95 @@ const months = [
 const letters = [
     {
       letter:'a',
-      names:['Perky', 'Happy', 'Adventurous']
+      names:['Perky', 'Happy', 'Adventurous', 'Gumball']
     },
     {
       letter:'b',
-      names:['Sparky', 'Bouncy', 'Jingle']
+      names:['Sparky', 'Bouncy', 'Jingle', 'Lollipop']
     },
     {
       letter:'c',
-      names:['Snow', 'Peppy', 'Cuddly']
+      names:['Snow', 'Peppy', 'Cuddly', 'Bubbly']
     },
     {
       letter:'d',
-      names:['Dainty', 'Winter', 'Cranberry']
+      names:['Dainty', 'Winter', 'Cranberry', 'Grumpy']
     },
     {
       letter:'e',
-      names:['Jangle', 'Squeezy', 'Energetic']
+      names:['Jangle', 'Squeezy', 'Energetic', 'Tumbles']
     },
     {
       letter:'f',
-      names:['Fearless', 'Jingle', 'Sunny']
+      names:['Fearless', 'Jingle', 'Sunny', 'Giggles']
     },
     {
       letter:'g',
-      names:['Merry', 'Winter', 'Flaky']
+      names:['Merry', 'Winter', 'Flaky', 'Cocoa']
     },
     {
       letter:'h',
-      names:['Tootsie', 'Spanky', 'Humble']
+      names:['Tootsie', 'Spanky', 'Humble', 'Brandy']
     },
     {
       letter:'i',
-      names:['Kringle', 'Peppermint', 'Innocent']
+      names:['Kringle', 'Peppermint', 'Innocent', 'Naughty']
     },
     {
       letter:'j',
-      names:['Puddin', 'Figgy', 'Jumpy']
+      names:['Puddin', 'Figgy', 'Jumpy', 'Bossy']
     },
     {
       letter:'k',
-      names:['Cookie', 'Glitter', 'Kindhearted']
+      names:['Cookie', 'Glitter', 'Kindhearted', "Ol'"]
     },
     {
       letter:'l',
-      names:['Tinker', 'Tinsel', 'Lively']
+      names:['Tinker', 'Tinsel', 'Lively', 'Crusty']
     },
     {
       letter:'m',
-      names:['Twinkle', 'Winter', 'Mighty']
+      names:['Twinkle', 'Winter', 'Mighty', 'Busy']
     },
     {
       letter:'n',
-      names:['Buddy', 'Twinkle', 'Nimble']
+      names:['Buddy', 'Twinkle', 'Nimble', 'Shiny']
     },
     {
       letter:'o',
-      names:['Elfie', 'Garland', 'Outgoing']
+      names:['Elfie', 'Garland', 'Outgoing', 'Zippy']
     },
     {
       letter:'p',
-      names:['Jingle', 'Icy', 'Playful']
+      names:['Jingle', 'Icy', 'Playful', 'Fancy']
     },
     {
       letter:'q',
-      names:['Snowflake', 'Ralphie', 'Quick']
+      names:['Snowflake', 'Ralphie', 'Quick', 'Gumbo']
     },
     {
       letter:'r',
-      names:['Jolly', 'Holly', 'Rosy']
+      names:['Jolly', 'Holly', 'Rosy', 'Salty']
     },
     {
       letter:'s',
-      names:['Elvis', 'Cheeky', 'Stampy']
+      names:['Elvis', 'Cheeky', 'Stampy', 'Toothy']
     },
     {
       letter:'t',
-      names:['Sugarplum', 'Vanilla', 'Thoughtful']
+      names:['Sugarplum', 'Vanilla', 'Thoughtful', 'Dancy']
     },
     {
       letter:'u',
-      names:['Peaches', 'Nipper', 'Unique']
+      names:['Peaches', 'Nipper', 'Unique', 'Prancy']
     },
     {
       letter:'v',
-      names:['Gingerbread', 'Flashy', 'Very Nice']
+      names:['Gingerbread', 'Flashy', 'Very Nice', 'Limber']
     },
     {
       letter:'w',
-      names:['Frisbee', 'Frosty', 'Wiggly']
+      names:['Frisbee', 'Frosty', 'Wiggly', 'Dandy']
     },
     {
       letter:'x',
@@ -150,11 +150,11 @@ const letters = [
     },
     {
       letter:'y',
-      names:['Pinky', 'Sprinkle', 'Youthful']
+      names:['Pinky', 'Sprinkle', 'Youthful', 'Snappy']
     },
     {
       letter:'z',
-      names:['Tinsel', 'Star', 'Zesty']
+      names:['Tinsel', 'Star', 'Zesty', 'Swanky']
     }
 ]
 
